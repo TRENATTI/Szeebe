c80158f3d75832d2310b12d4dc0cc77ceb21823d
Scrippy <50034413+Scrippy@users.noreply.github.com>
2021-07-21 20:53:35 -0500

    Added Archive for Version Control

---
+++ System/Archive/Versions/Version1/index.js
@@ -0,0 +1,44 @@
+// By ScriptIntelligence
+
+
+// Structure Setup
+
+// node_modules
+// modules
+// settings
+// events
+// startup
+// functions
+
+// // //
+
+//const fs = require('fs');
+//const discord = require('discord.js');
+
+// // //
+
+//const clientSystem = './System/Client';
+
+// // //
+
+//const token = process.env.TOKEN;
+//const prefix = process.env.PREFIX;
+
+//
+
+//const client = new discord.Client();
+
+//
+
+//client.login(token);
+
+//
+
+//const clientFiles = fs
+//	.readdirSync(clientSystem)
+//	.filter(file => file.endsWith('.js'));
+
+//for (const file of clientFiles) {
+//	const clientFile = require(clientSystem + '/' + file);
+//	clientFile(client);
+//}
