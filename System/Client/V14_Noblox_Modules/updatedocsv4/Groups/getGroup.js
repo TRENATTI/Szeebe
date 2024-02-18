41d84a8aa5de02a88f1935010102920a4cf991dd
Satrience <50034413+SATRIENCE@users.noreply.github.com>
2024-02-18 13:19:12 -0600

    [WIP] Dynamic Demo -> getGroup.js

--- System/Client/V14_Noblox_Modules/updatedocsv4/Groups/getGroup.js
+++ System/Client/V14_Noblox_Modules/updatedocsv4/Groups/getGroup.js
@@ -1,3 +1,23 @@
+
+require("dotenv").config();
+const fs = require("fs");
+const axios = require("axios");
+const {
+	SlashCommandBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	EmbedBuilder,
+} = require("discord.js");
+const wait = require("node:timers/promises").setTimeout;
+const { query } = require("express");
+const { json } = require("stream/consumers");
+const e = require("express");
+
+const GroupTypeLibrarySingleParam = [
+    "getGroup"
+]
+
 module.exports = {
 	name: "getGroup",
     description: "N/A",
@@ -5,6 +25,137 @@ module.exports = {
     argsOptional: false,
     socketType: "groups",
 	async execute(interaction, noblox, admin) {
+        var db = admin.database();
+        const isReady = db.ref("szeebe/alapha-universe-docs-signature/Groups/getGroup");
 
+        // Attach an asynchronous callback to read the data at our posts reference
+        isReady.once("value", (snapshot) => {
+            console.log(snapshot.val());
+            if (snapshot.val()) {
+                isReady.set(false);
+                start();
+            }
+        });
+        async function start() {
+            const userdata = [];
+				var ref = db
+					.ref("szeebe")
+					.child("alapha-universe-docs")
+					.child("groups");
+				ref.once("value", (snapshot) => {
+					snapshot.forEach((childSnapshot) => {
+						var childKey = childSnapshot.key;
+						var childData = childSnapshot.val();
+						console.log(childKey, childData);
+						userdata.push({ childKey, childData });
+					});
+                    for (let i = 0; i < GroupTypeLibrarySingleParam.length; i++) {
+					    checkGroupsSingleParam(GroupTypeLibrarySingleParam[i], userdata);
+                    }
+				});
+                async function checkGroupsSingleParam(GroupType, userData) {//(userData) {
+                    console.log(GroupType);
+                    for (let i = 0; i < userData.length; i++) {
+                        setTimeout(async function timer() {
+                            try {
+                                const groupInfo = await noblox[GroupType](
+                                    Number(userData[i].childData.groupId)
+                                );
+                                console.log(groupInfo);
+                                getGroupGithub(groupInfo, GroupType);
+                            } catch {
+                                console.log(
+                                    `Error getting group info for ${userData[i].childKey} [${userData[i].childData.groupId}.`
+                                );
+                            }
+                            async function getGroupGithub(groupInfo, GroupType) {
+        
+                                try {
+                                    const res = await axios.get(
+                                        `https://raw.githack.com/Alpha-Authority/alapha-universe-docs/main/Docs/Groups/${userData[i].childKey}/${GroupType}.json`
+                                    )
+                                    if (res.data == null) {
+                                        continueGroupData(false, "", groupInfo, GroupType)
+                                    } else {
+                                        console.log(res.data)
+                                        continueGroupData(true, res, groupInfo, GroupType)
+                                    }
+                                } catch (error) {
+                                    const res = ""
+                                    continueGroupData(false, res, groupInfo, GroupType)
+                                }
+                            }
+                            async function continueGroupData(Flag, res, groupInfoCont, GroupType){
+                                if (Flag == true) {
+                                    var newdatastring = JSON.stringify(res.data);
+                                    var newdatajson = JSON.parse(newdatastring);
+                                    var newdatajsonstring = JSON.stringify(newdatajson);
+                                    const file = fs.writeFileSync(
+                                        `../alapha-universe-docs/Docs/Groups/${userData[i].childKey}/${GroupType}.json`,
+                                        newdatajsonstring,
+                                        "utf8"
+                                    )
+                                    const datafile2 = require(`../../../../../../alapha-universe-docs/Docs/Groups/${userData[i].childKey}/${GroupType}.json`);
+                                    var str = JSON.stringify(datafile2);
+                                    var strgD = JSON.stringify(groupInfoCont);
+                                    console.log(str != strgD)
+                                    if (str != strgD) {
+                                        const file2 = fs.writeFileSync(
+                                            `../alapha-universe-docs/Docs/Groups/${userData[i].childKey}/${GroupType}.json`,
+                                            strgD,
+                                            "utf8"
+                                        );
+        
+                                    } else {
+                                        console.log(false)
+                                    }
+        
+                                } else {
+                                    var strgD = JSON.stringify(groupInfoCont);
+                                    const file2 = fs.writeFile(
+                                        `../alapha-universe-docs/Docs/Groups/${userData[i].childKey}/${GroupType}.json`,
+                                        strgD, 
+                                        function(err) {
+        
+                                            console.log(err);
+                                            
+                                        }
+                                    );
+                                }
+                                if (i == userData.length - 1) {
+                                    updateDb()
+                                }
+                            }
+                        }, i * 7500);
+                    }
+                }
+                async function updateDb(){ 
+                    isReady.set(true);
+                    if (pushDebounce == true) {
+                        pushDebounce = false
+                        const util = require("node:util");
+                        const exec = util.promisify(
+                            require("node:child_process").exec
+                        );
+    
+                        async function gitPush() {
+                            var spawn = require("child_process").spawn;
+                            var npm = (process.platform === "win32" ? "exec_aud.bat" : "bat"),
+                            child = spawn(npm, [""]);
+                            child.stdout.on('data', function (data) { console.log(data.toString()); });
+                            child.stderr.on('data', function (data) { console.log(data.toString()); });
+                            child.on('error', function() { console.log(arguments); });
+    
+                        }
+                        gitPush();
+            
+                        db.ref("szeebe/alapha-universe-docs-ready").set(
+                            true
+                        );
+                        isReady.set(false);
+                        pushDebounce = true
+                    }
+                }
+        }
     }
 }
