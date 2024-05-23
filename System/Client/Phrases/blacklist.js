af4bfc1979aa1e8e50ad53ef3ded40698419c782
Satrience <50034413+SATRIENCE@users.noreply.github.com>
2024-05-22 19:32:54 -0500

    Vak sucks

--- System/Client/Phrases/blacklist.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: "!blacklist",
-	aliases: [],
-	execute(message) {
-		return message.channel.send(
-			"https://docs.google.com/spreadsheets/d/1gn5SAoMtV32fDlgTEIKlA6-_lUVQK44yg8Iv6qkCOho/edit"
-		);
-	},
-};
