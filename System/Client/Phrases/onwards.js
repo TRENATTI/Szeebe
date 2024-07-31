e3fc48f49196d72d24d251b2b939b86b05b7fdd2
Satrience <50034413+SATRIENCE@users.noreply.github.com>
2024-07-31 15:13:37 -0500

    Vyyrahh good // Create Phrases ->onwards.js & sportsfan.js // Update messages.js

---
+++ System/Client/Phrases/onwards.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "onwards",
+	aliases: [],
+	execute(message) {
+		return message.channel.send("https://tenor.com/view/capybara-crocodile-alligator-riding-on-the-way-gif-27496959");
+	},
+};
---
+++ System/Client/Phrases/sportsfan.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: "sportsfan",
+	aliases: [],
+	execute(message) {
+		return message.channel.send(`https://tenor.com/view/yapping-yapper-level-high-gif-16328718415342092358`);
+	},
+};
