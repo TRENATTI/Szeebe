f7bb5ce255e0868f69872e09edc1de716647b0f9
Satrience <50034413+VLALENCE@users.noreply.github.com>
2024-10-08 23:47:48 -0500

    Update CS | Prettify

--- System/Client/welcome-service.js
+++ System/Client/welcome-service.js
@@ -3,46 +3,46 @@ const fs = require("fs");
 const Discord = require("discord.js");
 
 function WS(client) {
-  let i = 0;
-  const photos = [
-    "https://cdn.discordapp.com/attachments/809592756946206751/1001874995577892974/welcomegayhaha.gif",
-    "https://cdn.discordapp.com/attachments/809592756946206751/1001874994768379914/welcgay3.gif",
-    "https://cdn.discordapp.com/attachments/809592756946206751/1001874994013421588/welcgay2.gif",
-  ];
+	let i = 0;
+	const photos = [
+		"https://cdn.discordapp.com/attachments/809592756946206751/1001874995577892974/welcomegayhaha.gif",
+		"https://cdn.discordapp.com/attachments/809592756946206751/1001874994768379914/welcgay3.gif",
+		"https://cdn.discordapp.com/attachments/809592756946206751/1001874994013421588/welcgay2.gif",
+	];
 
-  client.on("guildMemberAdd", (member) => {
-    if (!member.guild.id == 215221157937283075) return;
-    embedX = {
-      title: "",
-      color: 13193877,
-      description: `Welcome **${
-        member.user.username
-      }**! We are glad to see you here, Now you are officially part **of Alapha Universe**\n\nPlease read the #rules and also get some nice roles from #self-roles. **And don't forget to say hi!**\n\n**Account created on** <t:${Math.round(
-        member.user.createdTimestamp / 1000
-      )}:f>`,
-      timestamp: new Date(),
-      author: {
-        name: "",
-      },
-      image: {
-        url: `${photos[i++ % photos.length]}`,
-      },
-      thumbnail: {
-        url: "",
-      },
-      footer: {
-        text: `${member.user.tag} is our ${member.guild.memberCount} member • ID: ${member.id}`,
-      },
-      fields: [],
-    };
-    try {
-      member.guild.channels.cache
-      .get("710591152113451088")
-      .send({ content: `<@${member.id}>`,embeds: [embedX] })
-    } catch (err) {
-      console.log(new Date(), '| astolfo.js | ', err)
-    }
-  })
+	client.on("guildMemberAdd", (member) => {
+		if (!member.guild.id == 215221157937283075) return;
+		embedX = {
+			title: "",
+			color: 13193877,
+			description: `Welcome **${
+				member.user.username
+			}**! We are glad to see you here, Now you are officially part **of Alapha Universe**\n\nPlease read the #rules and also get some nice roles from #self-roles. **And don't forget to say hi!**\n\n**Account created on** <t:${Math.round(
+				member.user.createdTimestamp / 1000
+			)}:f>`,
+			timestamp: new Date(),
+			author: {
+				name: "",
+			},
+			image: {
+				url: `${photos[i++ % photos.length]}`,
+			},
+			thumbnail: {
+				url: "",
+			},
+			footer: {
+				text: `${member.user.tag} is our ${member.guild.memberCount} member • ID: ${member.id}`,
+			},
+			fields: [],
+		};
+		try {
+			member.guild.channels.cache
+				.get("710591152113451088")
+				.send({ content: `<@${member.id}>`, embeds: [embedX] });
+		} catch (err) {
+			console.log(new Date(), "| astolfo.js | ", err);
+		}
+	});
 }
 
 module.exports = WS;
