48aed1df6d357c2467913a402fc52ef415fbb333
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2024-10-16 01:02:09 -0500

    Update TS -> Add Developer Mode
    AAT & Ubuntu Revival

--- System/Client/ticket-service.js
+++ System/Client/ticket-service.js
@@ -11,6 +11,7 @@ async function TS(
 	prefix
 ) {
 	client.once("ready", async () => {
+		if (process.env.DEVELOPER_MODE) return;
 		const guild = await client.guilds.fetch(`793960635628781618`);
 		const channel = await guild.channels.fetch("1235751537901568070");
 		const response = await channel.messages.fetch("1235757142506602526");
